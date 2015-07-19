002fb39efa2273a22aa758ff520ec0d3c9fc6672
Fco Cardona <fracarc4@MacBook-de-Fco.local>
2015-07-19 17:30:42 +0200

    Añadido DB SQLite

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,3 +1,24 @@
+var models = require('../models/models.js');
+
+//GET /quizes/question
+exports.question = function(req,res){
+	models.Quiz.findAll().success(function(quiz){
+		res.render('quizes/question', {pregunta: quiz[0].pregunta});
+	});
+};
+
+//GET /quizes/answer
+exports.answer = function(req,res){
+	models.Quiz.findAll().success(function(quiz){
+		if(req.query.respuesta.toLowerCase() === quiz[0].respuesta.toLowerCase()){
+			res.render('quizes/answer', {respuesta: 'Correcto'});
+		}else{
+			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+		}
+	});
+};
+
+/*-----------------------------------------------------Before
 //GET /quizes/question
 exports.question = function(req,res){
 	res.render('quizes/question', {pregunta: 'Capital de Italia'});
@@ -11,3 +32,4 @@ exports.answer = function(req,res){
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
 };
+------------------*/
---
+++ models/quiz.js
@@ -0,0 +1,7 @@
+//Definición del modulo quiz
+module.exports = function(sequelize,DataTypes){
+	return sequelize.define('Quiz', 
+		{ pregunta: DataTypes.STRING,
+		  respuesta: DataTypes.STRING,
+		});
+}
